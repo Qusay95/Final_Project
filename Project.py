04c15cf4abe09d7b10f5352d0b455c572be9c4fb
unknown <qusay.ash1995@gmail.com>
2023-08-31 22:54:35 +0300

    4th commit

--- Project.py
+++ Project.py
@@ -43,7 +43,7 @@ class Member:
         member = library.find_member(member_id)
         book = library.find_book(book_id)
 
-        if member and book in member.borrowed_books:
+        if member and book in self.borrowed_books:
             self.borrowed_books.remove(book)
             book.is_available = True
             print(f"   -- {self.name} has successfully returned {book.title}.")
@@ -62,22 +62,34 @@ class Library:
 
     # TODO : implement Library methods
     def add_book(self, book):
-        pass
+        self.books.append(book)
 
     def add_member(self, member):
-        pass
+        self.members.append(member)
 
     def display_books(self):
-        pass
+        print(f"   -- ID\t|\t\tTitle\t\t|\t\tAuthor\t\t|\t\tLevel\t|\t\tAvailable")
+        print('-' * 90)
+        for book in self.books:
+            print(f"   -- {book.book_id}\t\t|\t\t{book.title}\t\t|\t\t{book.author}\t\t|\t\t{book.level}\t\t|\t\t{'Yes' if book.is_available else 'No'}")
 
     def find_book(self, book_id):
-        pass
+        for book in self.books:
+            if book.book_id == book_id:
+                return book
+        return None
 
     def display_members(self):
-        pass
+        print(f"   -- ID\t\t|\t\tName\t\t\t|\t\tEmail\t\t\t\t\t|\tLevel")
+        print('-' * 90)
+        for member in self.members:
+            print(f"   -- {member.member_id}\t\t\t|\t\t{member.name}\t\t\t|\t\t{member.email}\t\t|\t\t{member.level}")
 
     def find_member(self, member_id):
-        pass
+        for member in self.members:
+            if member.member_id == member_id:
+                return member
+        return None
 
 
 library = Library()
@@ -92,7 +104,55 @@ while True:
     print("7. Borrow Book")
     print("8. Return Book")
     print("9. Exit")
+    print(" ")
+    choice = int(input("Enter your choice: "))
 
-    choice = input("Enter your choice: ")
+    if choice == 1:
+        name = input(" * Enter member name : ")
+        email = input(" * Enter member email : ")
+        level = input(" * Enter member level : ")
+        member = Member(name, email, level)
+        library.add_member(member)
 
-# TODO : implement the menu
+    elif choice == 2:
+        # member_id = int(input(" * Enter Member ID: "))
+        # library.find_member(member_id)
+        # name = input(" * Enter member name : ")
+        # email = input(" * Enter member email : ")
+        # level = input(" * Enter member level : ")
+        # member = Member(name, email, level)
+        # library.(member)
+        pass
+
+    elif choice == 3:
+        library.display_members()
+
+    elif choice == 4:
+        pass
+
+    elif choice == 5:
+        title = input(" * Enter book title : ")
+        author = input(" * Enter book author : ")
+        level = input(" * Enter book level : ")
+        book = Book(title, author, level)
+        library.add_book(book)
+
+    elif choice == 6:
+        library.display_books()
+
+    elif choice == 7:
+        member_id = int(input(" * Enter Member ID: "))
+        book_id = int(input(" * Enter Book ID: "))
+        Member.borrow_book(member_id, book_id)
+
+    elif choice == 8:
+        member_id = int(input(" * Enter Member ID: "))
+        book_id = int(input(" * Enter Book ID: "))
+        Member.return_book(member_id, book_id)
+
+    elif choice == 9:
+        print("   -- Thank you.. Goodbye")
+        break
+
+    else:
+        print("   -- Invalid choice. Please put valid option.")

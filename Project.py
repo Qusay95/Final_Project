1bda8b4febf8555f1b7987edaa418eecd9f5a94e
unknown <qusay.ash1995@gmail.com>
2023-09-01 17:38:32 +0300

    Test commit

--- Project.py
+++ Project.py
@@ -22,37 +22,7 @@ class Member:
         self.borrowed_books = []
 
     # TODO : implement member method
-    def borrow_book(self, member_id, book_id):
-        member = library.find_member(member_id)
-        book = library.find_book(book_id)
-
-        if member and book and book.is_available and member.level == book.level:
-            self.borrowed_books.append(book)
-            book.is_available = False
-            print(f"   -- {self.name} has successfully borrowed {book.title}.")
-        elif not book:
-            print("   -- Book not found.")
-        elif not member:
-            print("   -- Member not found.")
-        elif not book.is_available:
-            print("   -- Book is not available.")
-        else:
-            print("   -- Member's level is not suitable for this book.")
-
-    def return_book(self, member_id, book_id):
-        member = library.find_member(member_id)
-        book = library.find_book(book_id)
-
-        if member and book in self.borrowed_books:
-            self.borrowed_books.remove(book)
-            book.is_available = True
-            print(f"   -- {self.name} has successfully returned {book.title}.")
-        elif not book:
-            print("   -- Book not found.")
-        elif not member:
-            print("   -- Member not found.")
-        else:
-            print("   -- Member did not borrow this book.")
+    # I will put ( borrow_book & return_book ) in class Library
 
 
 class Library:
@@ -91,6 +61,37 @@ class Library:
                 return member
         return None
 
+    def borrow_book(self, member_id, book_id):
+        member = self.find_member(member_id)
+        book = self.find_book(book_id)
+
+        if member and book and book.is_available and member.level == book.level:
+            member.borrowed_books.append(book)
+            book.is_available = False
+            print(f"   -- {member.name} has successfully borrowed {book.title}.")
+        elif not book:
+            print("   -- Book not found.")
+        elif not member:
+            print("   -- Member not found.")
+        elif not book.is_available:
+            print("   -- Book is not available.")
+        else:
+            print("   -- Member's level is not suitable for this book.")
+
+    def return_book(self, member_id, book_id):
+        member = self.find_member(member_id)
+        book = self.find_book(book_id)
+
+        if member and book in member.borrowed_books:
+            member.borrowed_books.remove(book)
+            book.is_available = True
+            print(f"   -- {member.name} has successfully returned {book.title}.")
+        elif not book:
+            print("   -- Book not found.")
+        elif not member:
+            print("   -- Member not found.")
+        else:
+            print("   -- Member did not borrow this book.")
 
 library = Library()
 print(' Welcome to the Library System '.center(100, '-'))
@@ -108,13 +109,16 @@ while True:
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
+        print(" # Add Member")
         name = input(" * Enter member name : ")
         email = input(" * Enter member email : ")
         level = input(" * Enter member level : ")
         member = Member(name, email, level)
         library.add_member(member)
+        print(" ")
 
     elif choice == 2:
+        print(" # Edit Member")
         member_id = int(input(" * Enter Member ID: "))
         library.find_member(member_id)
         name = input(" * Enter member name : ")
@@ -123,37 +127,50 @@ while True:
         member = Member(name, email, level)
         library.members.pop(member_id-1)
         library.members.insert(member_id-1, member)
-        pass
+        print(" ")
 
     elif choice == 3:
+        print(" # Show Members")
         library.display_members()
+        print(" ")
 
     elif choice == 4:
+        print(" # Delete Member")
         index = int(input(" * Enter Member ID: "))
         library.members.pop(index-1)
         print(f"   -- Member {index} is deleted ")
+        print(" ")
 
     elif choice == 5:
+        print(" # Add Book")
         title = input(" * Enter book title : ")
         author = input(" * Enter book author : ")
         level = input(" * Enter book level : ")
         book = Book(title, author, level)
         library.add_book(book)
+        print(" ")
 
     elif choice == 6:
+        print(" # Show Books")
         library.display_books()
+        print(" ")
 
     elif choice == 7:
+        print(" # Borrow Book")
         member_id = int(input(" * Enter Member ID: "))
         book_id = int(input(" * Enter Book ID: "))
-        Member.borrow_book(member_id, book_id)
+        library.borrow_book(member_id, book_id)
+        print(" ")
 
     elif choice == 8:
+        print(" # Return Book")
         member_id = int(input(" * Enter Member ID: "))
         book_id = int(input(" * Enter Book ID: "))
-        Member.return_book(member_id, book_id)
+        library.return_book(member_id, book_id)
+        print(" ")
 
     elif choice == 9:
+        print(" # Exit")
         print("   -- Thank you.. Goodbye")
         break
 
